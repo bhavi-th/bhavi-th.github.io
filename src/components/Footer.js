bb0c527215dd6d3a1fe32e93dbd4875639460489
Bhavith.S <bhaviths15@gmail.com>
2025-09-30 23:42:27 +0530

    Removed social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,10 @@
 import "./Footer.css";
-import Github from "../assets/icon/github.svg";
-import LinkedIn from "../assets/icon/linkedin.svg";
-import Mail from "../assets/icon/mail.svg";
-import Whatsapp from "../assets/icon/whatsapp.svg";
-import GithubInverted from "../assets/icon/github-inverted.svg";
-import LinkedInInverted from "../assets/icon/linkedin-inverted.svg";
-import MailInverted from "../assets/icon/mail-inverted.svg";
-import WhatsappInverted from "../assets/icon/whatsapp-inverted.svg";
 
 function Footer({ theme }) {
     return (
         <footer>
             <div>
                 <span id="name">Bhavith.S</span>
-                <ul id="social-links" className="fade-elements">
-                    <li><a href="https://github.com/bhavi-th" target="_blank" rel="noreferrer"><img src={theme ? GithubInverted : Github} alt="social links" /></a></li>
-                    <li><a href="https://www.linkedin.com/in/bhavith-s/" target="_blank" rel="noreferrer"><img src={theme ? LinkedInInverted : LinkedIn} alt="social links" /></a></li>
-                    <li><a href="mailto:bhaviths15@gmail.com" target="_blank" rel="noreferrer"><img src={theme ? MailInverted : Mail} alt="social links" /></a></li>
-                    <li><a href="https://wa.me/919901949627" target="_blank" rel="noreferrer"><img src={theme ? WhatsappInverted : Whatsapp} alt="social links" /></a></li>
-                </ul>
             </div>
             <p>
                 &copy; 2025 Bhavith. All rights reserved
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,9 @@
 import "./Home.css"
-import Github from "../assets/icon/github.svg";
-import LinkedIn from "../assets/icon/linkedin.svg";
-import Mail from "../assets/icon/mail.svg";
-import Whatsapp from "../assets/icon/whatsapp.svg";
-import GithubInverted from "../assets/icon/github-inverted.svg";
-import LinkedInInverted from "../assets/icon/linkedin-inverted.svg";
-import MailInverted from "../assets/icon/mail-inverted.svg";
-import WhatsappInverted from "../assets/icon/whatsapp-inverted.svg";
 
 function Home({ theme }) {
     return (
         <section id="home" className="Home">
             <span id="name" className="fade-elements">Bhavith S</span>
-            <ul id="social-links" className="fade-elements">
-                <li><a href="https://github.com/bhavi-th" target="_blank" rel="noreferrer"><img src={theme ? GithubInverted : Github} alt="social links" /></a></li>
-                <li><a href="https://www.linkedin.com/in/bhavith-s/" target="_blank" rel="noreferrer"><img src={theme ? LinkedInInverted : LinkedIn} alt="social links" /></a></li>
-                <li><a href="mailto:bhaviths15@gmail.com" target="_blank" rel="noreferrer"><img src={theme ? MailInverted : Mail} alt="social links" /></a></li>
-                <li><a href="https://wa.me/919901949627" target="_blank" rel="noreferrer"><img src={theme ? WhatsappInverted : Whatsapp} alt="social links" /></a></li>
-            </ul>
             <p className="fade-elements">
                 Welcome to my minimalist digi-space, where I craft clear and practical solutions to real-world problems. I believe technology should be purposeful—streamlining systems and making life easier, one line of code at a time.
             </p>
