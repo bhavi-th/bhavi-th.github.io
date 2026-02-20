885484567710a96c4ea49d126b318b6ee14c3b4e
Bhavith <bhaviths15@gmail.com>
2026-02-20 23:34:25 +0530

    Implimented black hole for hero section background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <HomePage />
     </>
   );
 };
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import "../styles/HeroPage.css";
+
+const HomePage = () => {
+  return (
+    <div className="HeroPage">
+      <div id="bg-circle"></div>
+      <div id="bg-line"></div>
+    </div>
+  );
+};
+
+export default HomePage;
