ad2d3cc27dfcbdc2406b3611a58a81e2fe66ca72
Bhavith <bhaviths15@gmail.com>
2026-02-21 21:49:20 +0530

    Added black hole to homepage

---
+++ src/components/BlackHole.jsx
@@ -0,0 +1,15 @@
+import "../styles/components/BlackHole.css";
+
+const BlackHole = () => {
+  return (
+    <div className="BlackHole">
+      <div className="black-hole-container">
+        <div id="gravitational-lens"></div>
+        <div id="event-horizon"></div>
+        <div id="accretion-disk"></div>
+      </div>
+    </div>
+  );
+};
+
+export default BlackHole;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,28 @@
-import "../styles/Navbar.css";
+import "../styles/components/Navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="Navbar">
-      <ul>
-        <li>
-          <Link to="/about">
-            <nobr>ABOUT ME</nobr>
-          </Link>
-        </li>
-        <li>
-          <Link to="/work">
-            <nobr>MY WORK</nobr>
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact">
-            <nobr>CONTACT ME</nobr>
-          </Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-container">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">
+              ABOUT ME
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/work" className="nav-link">
+              MY WORK
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link">
+              CONTACT ME
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
